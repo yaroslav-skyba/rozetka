bde0f63f311f6fdc4267a0b1350ba7f2117971bf
Yaroslav Skyba <yaroslav.skyba@nixsolutions.com>
2022-03-02 00:56:33 +0200

    Add

--- src/main/resources/public/js/profile/admin/creation.js
+++ src/main/resources/public/js/profile/admin/creation.js
@@ -3,15 +3,6 @@ onload = function () {
 }
 
 xmlHttpRequest.onreadystatechange = function () {
-    if (xmlHttpRequest.readyState === 4) {
-        if (xmlHttpRequest.status === 201) {
-            for (const formControlElement of formControlElements) {
-                localStorage.removeItem(formControlElement.id);
-            }
-
-            alert("success", "A user has been successfully created");
-        } else if (xmlHttpRequest.status === 409) {
-            alert("danger", "Some errors occurred while creating a user");
-        }
-    }
+    setXmlHttpRequest(201, creationStorageKeyPrefix, "A user has been successfully created",
+        "Some errors occurred while creating a user");
 }
